67d1a87ea40c319c7c7aa0515c8d6c9edda59277
Engramar Bollas <engramar.bollas@gmail.com>
2022-09-03 16:11:51 +1000

    Template version

---
+++ src/elements/Testimonial.jsx
@@ -0,0 +1,168 @@
+import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+class Testimonial extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <Tabs>
+                            <div className="row align-items-center">
+                                <div className="col-lg-6">
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>Jogn Due The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>Jogn Due </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>Fatima Asrafy standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>Fatima Asrafy </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>Jannin Jow The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>Jannin Jow </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>Standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>Johns Due </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>John Doe The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>John Doe </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>Chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>Orthi </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>Jaffin </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+
+                                    <TabPanel>
+                                        <div className="rn-testimonial-content text-left">
+                                            <div className="inner">
+                                                <p>Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
+                                            </div>
+                                            <div className="author-info">
+                                                <h6><span>JON CUMMINS </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                            </div>
+                                        </div>
+                                    </TabPanel>
+                                </div>
+                                <div className="col-lg-6 mt_md--40 mt_sm--40">
+                                    <TabList className="testimonial-thumb-wrapper">
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-1.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-2.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-3.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-4.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-5.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-6.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-7.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                        <Tab>
+                                            <div className="testimonial-thumbnai">
+                                                <div className="thumb">
+                                                    <img src="/assets/images/client/testimonial-8.jpg" alt="Testimonial Images"/>
+                                                </div>
+                                            </div>
+                                        </Tab>
+                                    </TabList>
+                                </div>
+                            </div>
+                        </Tabs>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+export default Testimonial;
---
+++ src/elements/contact/ContactThree.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import FormStyle from './FormStyle';
+
+class ContactThree extends Component {
+    render(){
+        return(
+            <div className="contact-form--1">
+                <div className="container">
+                    <div className="row row--35 align-items-start">
+                        <div className="col-lg-6 order-2 order-lg-1">
+                            <div className="section-title text-left mb--50">
+                                <span className="subtitle">Let's Say Hi</span>
+                                <h2 className="title">Hire Me.</h2>
+                                <div className="im_address_inner">
+                                    <div className="im_address">
+                                        <span>Call us directly:</span>
+                                        <a className="link im-hover" href="tel:+111 (0)55 5869 8976<">+111 (0)55 5869 8976</a>
+                                    </div>
+                                    <div className="im_address mt--5">
+                                        <span>Contact Email:</span>
+                                        <a className="link im-hover" href="mailto:example@gmail.com">example@gmail.com</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <FormStyle />
+                        </div>
+                        <div className="col-lg-6 order-1 order-lg-2">
+                            <div className="thumbnail mb_md--30 mb_sm--30">
+                                <img src={`${this.props.contactImages}`} alt="trydo"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default ContactThree;

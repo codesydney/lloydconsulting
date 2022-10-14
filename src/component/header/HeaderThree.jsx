d35c820119c79e6b682bcbd6b723bd0db6d1dbf6
Kathryn Kelly <kathryn@192-168-1-118.tpgi.com.au>
2022-10-14 13:39:08 +1100

    Update Navbar Instagram Link #22 TAKE 2

--- src/component/header/HeaderThree.jsx
+++ src/component/header/HeaderThree.jsx
@@ -87,7 +87,7 @@ class HeaderThree extends Component{
                         <div className="social-share-inner d-none d-sm-block">
                             <ul className="social-share social-style--2 color-black d-flex justify-content-start liststyle">
                                 {SocialShare.map((val , i) => (
-                                    <li key={i}><a href={`${val.link}`}>{val.Social}</a></li>
+                                    <li key={i}><a href={`${val.link}`} target="_blank" rel="noopener noreferrer">{val.Social}</a></li>
                                 ))}
                             </ul>
                         </div>

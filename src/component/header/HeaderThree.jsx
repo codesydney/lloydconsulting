04b48da8f849261c3472c9532c03791b366e0f90
Kathryn Kelly <kathryn@192-168-1-118.tpgi.com.au>
2022-10-17 09:04:00 +1100

    Change Merch to Shop nav menu item issues #26 and #28

--- src/component/header/HeaderThree.jsx
+++ src/component/header/HeaderThree.jsx
@@ -77,7 +77,7 @@ class HeaderThree extends Component{
                                 <li><a href="#home">Home</a></li>
                                 <li><a href="#about">About</a></li>
                                 <li><a href="#service">Services</a></li>
-                                <li><a href="#portfolio">Merch</a></li>
+                                <li><a href="#portfolio">Shop</a></li>
                                 <li><a href="#blog">Blog</a></li>
                                 <li><a href="#contact">Contact</a></li>
                             </Scrollspy>

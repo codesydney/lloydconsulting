c2e6b0d5a5b3de6893aafca0471bdd0adf6d7bed
davidtaing <adavidtaing@gmail.com>
2022-12-04 21:11:31 +1100

    reinstate: services section

--- src/dark/PortfolioLanding.jsx
+++ src/dark/PortfolioLanding.jsx
@@ -65,13 +65,29 @@ const PortfolioLanding = () => {
             </div>
             {/* End Slider Area   */} 
             
-                {/* Start Service Area  */}
-                <div className="service-area ptb--120 bg_color--5">
-                   <div className="container">
-                        <ServiceList />
-                   </div>
-                </div>
-                {/* End Service Area  */}
+               
+            {/* Start Service Area  */}
+            <div id="service" className="fix">
+                <div className="service-area creative-service-wrapper pb--120 bg_color--8" >
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="section-title text-center mb--30 mb_sm--0">
+                                    <span className="subtitle">How can I help</span>
+                                    <h2 className="title">Services</h2>
+                                    <p className="description">There are many variations of passages of Lorem Ipsum available, <br /> but the majority have suffered alteration.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row creative-service">
+                            <div className="col-lg-12">
+                                <ServiceList item="6" column="col-lg-4 col-md-6 col-sm-6 col-12 text-left" />
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        {/* End Service Area  */} 
 
             {/* Start Contact Area */}
             <div id="contact" className="fix">

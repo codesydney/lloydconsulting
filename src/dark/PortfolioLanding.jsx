181fdf2fe721d9523395356f8ac4233534c33900
n4heen <101859246+n4heen@users.noreply.github.com>
2022-12-05 18:58:27 +1100

    Revert "About page"

--- src/dark/PortfolioLanding.jsx
+++ src/dark/PortfolioLanding.jsx
@@ -22,33 +22,34 @@ const SlideList = [
     }
 ]
 const PortfolioLanding = () => {
-
-    const PostList = BlogContent.slice(0, 3);
+    let title = 'About Lloyd Consulting Co',
+        description = 'Lloyd Consulting Co is a non profit charity organisation from Dean M. Lloyd';
+    const PostList = BlogContent.slice(0 , 3);
     return (
         <div className="active-dark bg_color--9">
             <Helmet pageTitle="Portfolio Landing" />
 
-            <HeaderThree homeLink="/" logo="symbol-dark" color="color-black" />
-
+            <HeaderThree homeLink="/" logo="symbol-dark" color="color-black"/>
+            
             {/* Start Slider Area   */}
             <div id="home" className="fix">
                 <div className="slider-wrapper">
                     {/* Start Single Slide */}
-                    {SlideList.map((value, index) => (
+                    {SlideList.map((value , index) => (
                         <div className="slide personal-portfolio-slider slider-paralax slider-style-3 d-flex align-items-center justify-content-center bg_image bg_image--25" key={index}>
                             <div className="container">
                                 <div className="row">
                                     <div className="col-lg-12">
                                         <div className={`inner ${value.textPosition}`}>
                                             {value.category ? <span>{value.category}</span> : ''}
-                                            <h1 className="title">Lloyd Consulting Co <br />
-                                                <TextLoop>
-                                                    <span> Advocate.</span>
-                                                    <span> Counsellor.</span>
-                                                    <span> Humane.</span>
-                                                    <span> Lived Experience.</span>
-                                                    <span> Diversity Consultant.</span>
-                                                </TextLoop>{" "}
+                                            <h1 className="title">Lloyd Consulting Co <br/>
+                                            <TextLoop>
+                                                <span> Advocate.</span>
+                                                <span> Counsellor.</span>
+                                                <span> Humane.</span>
+                                                <span> Lived Experience.</span>
+                                                <span> Diversity Consultant.</span>
+                                            </TextLoop>{" "}
                                             </h1>
                                             <h2>Business Done Differently</h2>
                                             {value.description ? <p className="description">{value.description}</p> : ''}
@@ -62,9 +63,9 @@ const PortfolioLanding = () => {
                     {/* End Single Slide */}
                 </div>
             </div>
-            {/* End Slider Area   */}
-
-
+            {/* End Slider Area   */} 
+            
+               
             {/* Start Service Area  */}
             <div id="service" className="fix">
                 <div className="service-area creative-service-wrapper pb--120 bg_color--8" >
@@ -84,75 +85,41 @@ const PortfolioLanding = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>  
             </div>
-            {/* End Service Area  */}
+        {/* End Service Area  */} 
 
-            {/* Start About Area */}
-            <div id="about" className="fix">
-
-
-                {/* Start Contact Area */}
-                <div id="contact" className="fix">
-                    <div className="about-area ptb--120  bg_color--8">
-                        <div className="about-wrapper">
-                            <div className="container">
-                                <div className="row row--35">
-
-
-
-
-
-                                    <div className="col-lg-5">
-                                        <div className="thumbnail">
-                                            <img className="w-100" src="/assets/images/blog/newImageForBlog-01.jpg" alt="About Images" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-7">
-                                        <div className="about-inner inner">
-                                            <div className="section-title">
-                                                <span className="subtitle">About Details</span>
-                                                <h2 className="title mb--20">About Lloyd Consulting Co</h2>
-                                                <p className="description mt_dec--20">Lloyd Consulting Co understands the future of change management. With over a decade of experience in supporting people to transform their lives for the better, Lloyd Consulting Co knows what is required to sustain change. Lloyd Consulting Co specialises in providing a voice of lived experience for systemic change.
-                                                    <br />
-                                                    <br />
-                                                    We know how to unlock the value of diversity and celebrate difference. Lloyd Consulting Co has successfully developed a program for change. If you a ready for change, reach out and to learn how our Safety First Personal Responsibility approach can help you identify,  understand and implement a support system for change.</p>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {/* End About Area */}
-
-
-
+            {/* Start Contact Area */}
+            <div id="contact" className="fix">
+                <div className="about-area ptb--120  bg_color--8">
+                    <div className="about-wrapper">
+                        <div className="container">
+                            <div className="row row--35">
 
                 <div id="contact" className="fix">
                     <div className="rn-contact-area pb--120 bg_color--8">
                         <ContactThree contactImages="/assets/images/about/contactUs.jpg" contactTitle="Work with me." />
                     </div>
                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            
+            {/* End Contact Area */}
+
+
+            <FooterTwo />
+            {/* Start Back To Top */}
+            <div className="backto-top">
+                <ScrollToTop showUnder={160}>
+                    <FiChevronUp />
+                </ScrollToTop>
+            </div>
+            {/* End Back To Top */}
+            
         </div>
-
-
-        // {/* End Contact Area */ }
-
-
-        // < FooterTwo />
-        // {/* Start Back To Top */ }
-        // < div className = "backto-top" >
-        //     <ScrollToTop showUnder={160}>
-        //         <FiChevronUp />
-        //     </ScrollToTop>
-        //         </div >
-        // {/* End Back To Top */ }
-
     )
 }
 

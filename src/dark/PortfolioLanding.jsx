4914542bf0f994622a7ae0f2faa3b29a6dbaced9
Engramar Bollas <engramar.bollas@gmail.com>
2023-06-15 05:50:26 +1000

    Remove learn more button

--- src/dark/PortfolioLanding.jsx
+++ src/dark/PortfolioLanding.jsx
@@ -58,8 +58,10 @@ const PortfolioLanding = () => {
                                                 </h1>
                                             </TextLoop>
                                             <h2 className="hero-headline">Business Done Differently</h2>
-                                            {value.description ? <p className="description">{value.description}</p> : ''}
+                                            {value.description ? <p className="description">{value.description}</p> : ''} 
+                                            {/*                                          
                                             {value.buttonText ? <div className="slide-btn mt--30"><a className="btn-default btn-border btn-opacity" href={`${value.buttonLink}`}>{value.buttonText}</a></div> : ''}
+                                            */}
                                         </div>
                                     </div>
                                 </div>
